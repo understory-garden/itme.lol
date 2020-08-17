7d3ac8bf61d698962668993c620385f53ef3a7a0
Travis Vachon <travis.vachon@gmail.com>
2020-08-17 12:32:22 -0700

    monetize to USD

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
         <title>adventure</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://use.typekit.net/dtu4zwf.css" />
-        <meta name="monetization" content="$ilp.uphold.com/fR3aBhBwnnjy" />
+        <meta name="monetization" content="$ilp.uphold.com/DYPhbXPmDa2P" />
       </Head>
       <AuthenticationProvider>
         <Component {...pageProps} />
