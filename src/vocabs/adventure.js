c3593c343e20d94d032d5ce386acb4164bc58ebb
Travis Vachon <travis.vachon@gmail.com>
2020-08-16 17:19:49 -0700

    fix vocab
    I had so many extra # !

--- src/vocabs/adventure.js
+++ src/vocabs/adventure.js
@@ -1,13 +1,13 @@
 const adv = `https://itme.lol/v/adventure#`
 export default ({
-  firstAction: `${adv}#firstAction`,
-  Door: `${adv}#Door`,
-  from: `${adv}#from`,
-  to: `${adv}#to`,
-  north: `${adv}#north`,
-  west: `${adv}#west`,
-  south: `${adv}#south`,
-  east: `${adv}#east`,
-  up: `${adv}#up`,
-  down: `${adv}#down`
+  firstAction: `${adv}firstAction`,
+  Door: `${adv}Door`,
+  from: `${adv}from`,
+  to: `${adv}to`,
+  north: `${adv}north`,
+  west: `${adv}west`,
+  south: `${adv}south`,
+  east: `${adv}east`,
+  up: `${adv}up`,
+  down: `${adv}down`
 })
